5081db56edb264104ac4105af278e2ea3376d8e5
D4RKGEEK <vaibhav97825@gmail.com>
2026-01-25 13:53:43 +0530

    image fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add any specific config here if needed
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
